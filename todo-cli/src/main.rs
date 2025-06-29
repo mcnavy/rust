78d33a83f43e94d97f174ee132c2bb32030a1adc
mcnavy <a.panaiotidi@vk.team>
2025-06-29 05:37:56 +0300

    colored

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -4,6 +4,7 @@ mod storage;
 mod task_manager;
 
 use clap::Parser;
+use colored::*;
 use models::Task;
 use cli::{Cli, Commands};
 use storage::{save_tasks, load_tasks};
@@ -16,32 +17,35 @@ fn main() {
     match cli.command {
         Commands::Add { title } => {
             let new_id = get_next_id(&tasks);
-            println!("Adding task: {} with ID: {}", title, new_id);
 
             tasks.push(Task { id: new_id, title, completed: false });
             
             save_tasks(&tasks);
 
-            println!("Task added successfully");
+            println!("{}", "Task added successfully".green());
         },
         Commands::List => {
             if tasks.is_empty() {
-                println!("No tasks found");
+                println!("{}", "Task not found".red());
             } else {
                 for task in tasks {
-                    println!("ID: {}, Title: {}, Completed: {}", task.id, task.title, task.completed);
+                    if task.completed {
+                        println!("✅ {} {}", task.id, task.title.green());
+                    } else {
+                        println!("⏳ {} {}", task.id, task.title.white());
+                    }
                 }
             }
         },
         Commands::Complete { id } => {
             mark_task_as_completed(&mut tasks, id);
             save_tasks(&tasks);
-            println!("Task completed successfully");
+            println!("{}", "Task completed successfully".blue());
         },
         Commands::Remove { id } => {
             remove_task(&mut tasks, id);
             save_tasks(&tasks);
-            println!("Task removed successfully");
+            println!("{}", "Task removed successfully".yellow());
         }
     }
 }
